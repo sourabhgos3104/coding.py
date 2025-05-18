585a8c7797358a8df282f50e551d1a9cf3a345d6
sourabhgos3104 <sourabhgoswmi3104@gmail.com>
2025-05-18 10:30:57 +0530

    Add files via upload

---
+++ class11.py
@@ -0,0 +1,19 @@
+li=[110,51,82,55,89,76,45,25,33]
+
+max=li[0]
+for i in range(len(li)):
+    if max<li[i]:
+        max=li[i]
+
+print(max)      
+
+
+li=[110,51,82,55,89,76,45,25,33]
+
+max=li[0]
+for i in range(len(li)):
+    if max>li[i]:
+        max=li[i]
+
+print(max)
+
