da6fa51b46e404b0e4e4dfa240b8b85aa83c9c45
sourabhgos3104 <sourabhgoswmi3104@gmail.com>
2025-05-18 10:30:33 +0530

    Add files via upload

---
+++ class10.py
@@ -0,0 +1,24 @@
+a=10
+b=20
+a=a*b
+b=a/b
+a=a/b
+
+print(a)
+print(b)
+
+
+
+list=[1,2,3,4,5,7,8,9]
+
+print(list[::-1])
+
+for i in range(1,10):
+     print(i)
+
+li=[3,6,7,8,9]
+
+print(len(li))
+for i in range(len(li)):
+    print(li)
+
